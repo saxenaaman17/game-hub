551feaa5bf55dfca3fa9779ff45cbe124f855571
aman28 <aman@praemineo.com>
2023-09-27 23:48:44 +0530

    feat(App.tsx): build a responsive layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,25 @@
-import { Button } from "@chakra-ui/react";
+import { Grid, GridItem, Show } from "@chakra-ui/react";
 
 function App() {
   return (
-    <>
-      <Button colorScheme="blue">Click me!</Button>
-    </>
+    <Grid
+      templateAreas={{
+        base: `"nav" "main"`,
+        lg: `"nav nav" "aside main"`, // greater than 1024px
+      }}
+    >
+      <GridItem area="nav" bg="lightblue">
+        Nav
+      </GridItem>
+      <Show above="lg">
+        <GridItem area="aside" bg="lightgreen">
+          aside
+        </GridItem>
+      </Show>
+      <GridItem area="main" bg="lightgrey">
+        main
+      </GridItem>
+    </Grid>
   );
 }
 
