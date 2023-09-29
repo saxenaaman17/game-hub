9ea6185fa6b62828e45bb63f3a5e6847124937d9
aman28 <saxenaaman17@gmail.com>
2023-09-30 03:37:43 +0530

    add GameCardSkeleton component to show skeleton ui while fetching games from api

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -24,15 +24,21 @@ interface FetchGamesResponse {
 const useGames = () => {
   const [games, setGames] = useState<Game[]>([]);
   const [error, setError] = useState("");
+  const [isLoading, setLoading] = useState(false);
 
   useEffect(() => {
     const controller = new AbortController();
+    setLoading(true);
     apiClient
       .get<FetchGamesResponse>("/games", { signal: controller.signal })
-      .then((res) => setGames(res.data.results))
+      .then((res) => {
+        setGames(res.data.results);
+        setLoading(false);
+      })
       .catch((err) => {
         if (err instanceof CanceledError) return;
         setError(err.message);
+        setLoading(false);
       });
 
     return () => {
@@ -40,7 +46,7 @@ const useGames = () => {
     };
   }, []);
 
-  return { games, error };
+  return { games, error, isLoading };
 };
 
 export default useGames;
