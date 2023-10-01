80d6c43f32d8b16f690c8f01bf677006fe4d59b3
aman28 <saxenaaman17@gmail.com>
2023-10-01 16:37:29 +0530

    add skeleton ui for loading state in genre list component

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,29 +1,32 @@
 import { HStack, Image, List, ListItem, Text } from "@chakra-ui/react";
 import useGenres from "../hooks/useGenres";
 import getCroppedImageUrl from "../services/image-url";
+import GenreListSkeleton from "./GenreListSkeleton";
 
 const GenreList = () => {
-  const { data } = useGenres();
+  const { data, isLoading, error } = useGenres();
+
+  if (error) return null;
+
+  if (isLoading) return <GenreListSkeleton />;
 
   return (
-    <>
+    <List>
       {data.map((genre) => (
-        <List key={genre.id} spacing={3}>
-          <ListItem paddingY="4px">
-            <HStack spacing={4}>
-              <Image
-                src={getCroppedImageUrl(genre.image_background)}
-                boxSize="32px"
-                borderRadius="6px"
-                objectFit="cover"
-                alt="genre icon"
-              />
-              <Text fontSize="lg">{genre.name}</Text>
-            </HStack>
-          </ListItem>
-        </List>
+        <ListItem key={genre.id} paddingY="4px">
+          <HStack spacing={4}>
+            <Image
+              src={getCroppedImageUrl(genre.image_background)}
+              boxSize="32px"
+              borderRadius="6px"
+              objectFit="cover"
+              alt="genre icon"
+            />
+            <Text fontSize="lg">{genre.name}</Text>
+          </HStack>
+        </ListItem>
       ))}
-    </>
+    </List>
   );
 };
 
---
+++ src/components/GenreListSkeleton.tsx
@@ -0,0 +1,26 @@
+import {
+  HStack,
+  List,
+  ListItem,
+  Skeleton,
+  SkeletonText,
+} from "@chakra-ui/react";
+
+const GenreListSkeleton = () => {
+  const skeletons = [1, 2, 3, 4, 5, 6];
+  
+  return (
+    <List>
+      {skeletons.map((id) => (
+        <ListItem key={id} paddingY="4px">
+          <HStack spacing={4}>
+            <Skeleton boxSize="32px" borderRadius="6px" />
+            <SkeletonText noOfLines={1} width="100px" skeletonHeight="4" />
+          </HStack>
+        </ListItem>
+      ))}
+    </List>
+  );
+};
+
+export default GenreListSkeleton;
