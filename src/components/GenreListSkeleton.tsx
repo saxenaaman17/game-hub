c2367928554eaa89580de8b7f8f9819c72e24ebf
aman28 <saxenaaman17@gmail.com>
2023-10-01 22:30:38 +0530

    add platform selector dropdown above GameGrid component

--- src/components/GenreListSkeleton.tsx
+++ src/components/GenreListSkeleton.tsx
@@ -7,7 +7,7 @@ import {
 } from "@chakra-ui/react";
 
 const GenreListSkeleton = () => {
-  const skeletons = [1, 2, 3, 4, 5, 6];
+  const skeletons = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
   
   return (
     <List>
